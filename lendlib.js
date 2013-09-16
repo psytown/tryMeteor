d2976d67e99d2941fab21f023d36354b3a982410
Sean Zhang <psytown@qq.com>
2013-09-16 19:02:42 +0800

    first commit

---
+++ lendlib.js
@@ -0,0 +1,44 @@
+Lists = new Meteor.Collection("lists");
+
+if (Meteor.isClient) {
+
+  Template.categories.Lists = function (){
+     return Lists.find({},{sort:{Category:1}});
+  };
+
+  Session.set('adding_category',false);
+
+  Template.categories.new_cat = function (){
+    return Session.equals('adding_category',true);
+  };
+
+  Template.categories.events({
+    'click #btnNewCat' : function (e,t){
+      Session.set('adding_category',true);
+      Meteor.flush();
+      focusText(t.find("#add-category"));
+    },
+    'keyup #add-category': function(e,t){
+      if (e.which === 13){
+        var catVal = String(e.target.value || "");
+        if (catVal){
+          Lists.insert({Category:catVal});
+          Session.set('adding_category',false);
+        }
+      }
+    }
+  });
+
+  function focusText(i){
+    i.focus();
+    i.select();
+  };
+
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
